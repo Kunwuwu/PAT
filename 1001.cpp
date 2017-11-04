036c620830a19b88d07ae8dab49e2a2e2e95e2d6
感谢您的配合 <31055329+Kunwuwu@users.noreply.github.com>
2017-11-04 12:34:07 +0800

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b, count = 0, i = 0;        // count用于计数
+    char str[20];
+    cin >> a >> b;
+    sprintf(str, "%d", (a+b));         // 将整型转化成字符数组，需要头文件cstdio
+    string s = str;                    // 将字符数组转化成字符串类
+    int len = s.length();              // 使用len变量储存字符串长度
+    if(s[0] == '-')                    // 如果A+B和是负数，则输出‘-’，len记录数字的长度
+    {
+        len--;
+        i = 1;                         // 使对字符串的遍历直接从数字开始
+        cout << '-';                   // 输出‘-’，接下来直接处理数字就行
+    }
+    for (i; i < s.length(); i++) {        
+        if((len - count) % 3 ==0 && count > 0)
+            cout << ',';               // 如果从最后一个字符开始数，这个字符位置是3的的倍数，输出逗号
+        cout << s[i];                         
+        count++;
+    }
+    return 0;
+}
