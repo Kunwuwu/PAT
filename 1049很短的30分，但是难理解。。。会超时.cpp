e7a36e5014ba4dcbffdd1b8fe50d457539676f58
宝宝可乖了 <31055329+Kunwuwu@users.noreply.github.com>
2018-01-29 16:58:23 +0800

    Create 1049很短的30分，但是难理解。。。会超时.cpp

---
+++ 1049很短的30分，但是难理解。。。会超时.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>                           /* 因为超时，讲真，思路有点难理解 */
+#include <iostream>                         /* 感觉这个规律让直接推导出来也是件不简单的事情 */
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int now, left, right, a = 1, ans = 0;
+    while(n / a)
+    {
+        left = n / (a * 10), now = (n / a) % 10, right = n % a;
+        if(now == 0)
+            ans += left * a;
+        else if(now == 1)
+            ans += (left * a + right + 1);
+        else
+            ans += (left + 1) * a;
+        a *= 10;
+    }
+    cout << ans;
+    return 0;
+}
