07067d6bb5a4dde23bde7c19ddccf4ae769b0ed5
宝宝可乖了 <31055329+Kunwuwu@users.noreply.github.com>
2018-01-29 17:08:30 +0800

    Update 1049很短的30分，但是难理解。。。会超时.cpp

--- 1049很短的30分，但是难理解。。。会超时.cpp
+++ 1049很短的30分，但是难理解。。。会超时.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>                           /* 因为超时，讲真，思路有点难理解 */
+#include <cstdio>                           /* 因为超时，讲真，思路有点难理解，这种题目还是只能拿22分 */
 #include <iostream>                         /* 感觉这个规律让直接推导出来也是件不简单的事情 */
 
 using namespace std;
@@ -10,7 +10,7 @@ int main()
     int now, left, right, a = 1, ans = 0;
     while(n / a)
     {
-        left = n / (a * 10), now = (n / a) % 10, right = n % a;
+        left = n / (a * 10), now = (n / a) % 10, right = n % a;        /* 注意这now的计算方法 */
         if(now == 0)
             ans += left * a;
         else if(now == 1)
