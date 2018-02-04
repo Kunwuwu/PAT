d08f4a857062c9bb098c06be6ab2451074950760
宝宝可乖了 <31055329+Kunwuwu@users.noreply.github.com>
2018-02-04 16:21:10 +0800

    Create 1065longlong溢出.cpp

---
+++ 1065longlong溢出.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        long long a, b, c;
+        string ans;
+        cin >> a >> b >> c;
+        long long sum = a + b;                          /* c不可能溢出 */
+        if(a > 0 && b > 0 && sum < 0)                   /* 如果a,b都大于零，而sum溢出的话，肯定大于c，sum的值为负 */
+            ans = "true";
+        else if(a < 0 && b < 0 && sum >= 0)             /* a,b为负，sum溢出，sum应该小于c，sum实际为非负数， */
+            ans = "false";
+        else if(sum > c)
+            ans = "true";
+        else
+            ans = "false";
+        printf("Case #%d: ", i);
+        cout << ans << endl;
+    }
+    return 0;
+}
