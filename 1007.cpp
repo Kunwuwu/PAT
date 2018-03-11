27287137a28110ba86e99393b175ca5fb07e5f6c
宝宝可乖了 <31055329+Kunwuwu@users.noreply.github.com>
2018-03-11 10:52:12 +0800

    Update 1007.cpp

--- 1007.cpp
+++ 1007.cpp
@@ -1,65 +1,37 @@
+#include <cstdio>
 #include <iostream>
 #include <vector>
-#include <cstdio>
 
 using namespace std;
 
 vector<int> v;
-
-int MaxSubseqSum(vector<int> v, int N);
-int IsAllZero(vector<int> v);
-int in = 0;
-int out = 0;
+int n;
 
 int main()
 {
-    int N, e, MaxSum;
-    scanf("%d", &N);
-    for(int i = 0; i < N; i++)
+    int maxsum = -1;                                      /* 这个一定要小于零，不然会有一个测点过不了，如-1 -1 0 -1 */
+    int tempsum = 0;
+    cin >> n;
+    v.resize(n);
+    int left = 0, right = n - 1, tleft = 0, tright = 0;   /* 如果是全负的情况，则left和right不会更新，这样代表整个序列 */
+    bool flag = false;
+    for(int i = 0; i < n; i++) scanf("%d", &v[i]);
+    for(int i = 0;i < n; i++)
     {
-        scanf("%d", &e);
-        v.push_back(e);
-    }
-    MaxSum = MaxSubseqSum(v, N);
-    if(MaxSum == 0 && IsAllZero(v))               // 判断是否所有的元素都为零的特殊情况
-    {
-        in = 0;
-        out = v.size() - 1;
-    }
-    printf("%d %d %d", MaxSum, v[in], v[out]);
-    return 0;
-}
-
-int MaxSubseqSum(vector<int> v, int N)
-{
-    int MaxSum, ThisSum;
-    MaxSum = ThisSum =  0;
-    int temp = 0;
-    for(int i = 0; i < N; i++)
-    {
-        ThisSum += v[i];
-        if(ThisSum > MaxSum || ( ThisSum == MaxSum && v[i - 1] < 0))
-        {                                         // 防止类似于-1 0 0 -1的情况出现
-            MaxSum = ThisSum;
-            out = i;
-            in = temp;                            // 如果出现了更大的子序列和，则更新序列的起止下标
-        }
-        else if(ThisSum < 0)
+        tempsum += v[i];
+        tright = i;
+        if(tempsum > maxsum)
         {
-            ThisSum = 0;                          // 如果当前子序列的和小于零了，则意味着后面数能够成更大的子序列
-            temp = i + 1;                         // 将新子序列的始下标更新
+            maxsum = tempsum;
+            left = tleft;
+            right = tright;
+        }
+        if(tempsum < 0)
+        {
+            tempsum = 0;
+            tleft = i + 1;
         }
     }
-    return MaxSum;
-}
-
-int IsAllZero(vector<int> v)
-{
-    bool flag = true;
-    for(auto it = v.cbegin(); it != v.cend(); it++)
-    {
-        if(*it >= 0)
-            flag = false;
-    }
-    return flag;
+    if(maxsum < 0) maxsum = 0;
+    printf("%d %d %d", maxsum, v[left], v[right]);
 }
